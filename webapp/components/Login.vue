8e9edef690225b2a30da53dae7ee493302d4012b
Soham Tamboli <sohamtamboli@gmail.com>
2020-06-16 19:24:01 +0530

    register pages
    Added Login component
    Added SignUp component
    !! kindly note testregi route is testing purposes delete after feature is fully completed.

---
+++ webapp/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <span class="headline">Log in</span>
+    </v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field v-model="name" label="Email*" required></v-text-field>
+          </v-col>
+          <!-- <v-col cols="12"> {{ name }} </v-col> -->
+          <v-col cols="12">
+            <v-text-field
+              label="Password*"
+              type="password"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+      <small>*indicates required field</small>
+      <p class="text-lg-right">
+        New User?<v-btn @click="changeOption" text color="orange darken-1">
+          Create Account
+        </v-btn>
+      </p>
+    </v-card-text>
+    <v-card-actions>
+      <!-- <v-spacer></v-spacer> -->
+      <v-btn
+        color="orange darken-1"
+        text
+        @click="dialog = false"
+        class="mx-auto"
+        >Log in</v-btn
+      >
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    changeOption() {
+      this.$emit('optionChanged')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ webapp/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-card>
+        <v-card-title>
+          <span class="headline">Sign Up!</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              
+              <v-col cols="12">
+                <v-text-field label="Email*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-col>
+              
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+          <p class="text-lg-right">Already an User?<v-btn @click="changeOption" text color="orange darken-1"> Log in </v-btn> </p>
+        </v-card-text>
+        <v-card-actions>
+          <!-- <v-spacer></v-spacer> -->
+          <v-btn color="orange darken-1" text @click="dialog = false" class="mx-auto">Sign Up</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        methods: {
+            changeOption(){
+                this.$emit('optionChanged');
+            }
+            
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
