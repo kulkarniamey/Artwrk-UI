6b04e864feaf741a917fa45c6d4495633d3c6b4e
ZeffK <54438151+ZeffK@users.noreply.github.com>
2020-06-16 16:37:13 +0530

    Update default.vue

--- webapp/layouts/default.vue
+++ webapp/layouts/default.vue
@@ -23,16 +23,24 @@
     </v-content>
 
     <v-footer
+    v-row justify="center" rows="12"
 
     dark
     padless
+    
+    
   >
+
+    <v-row justify="center" no-gutters>
+     <v-col >
     <v-card
       flat
       tile
       class="indigo lighten-1 white--text text-center"
+      
     >
-      <v-card-text>
+      <v-card-text > 
+        
         <v-btn
           v-for="icon in icons"
           :key="icon"
@@ -43,16 +51,18 @@
         </v-btn>
       </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </v-card-text>
+     
 
       <v-divider></v-divider>
+      
 
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      <v-card-text class="white--text" >
+        {{ new Date().getFullYear() }} — <strong>ArtWrk</strong>
       </v-card-text>
     </v-card>
+
+      </v-col>
+    </v-row>
   </v-footer>
 
 
