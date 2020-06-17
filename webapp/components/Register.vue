78050a74bd3cb04aeef4220db98ea0a6045ca619
Soham Tamboli <sohamtamboli@gmail.com>
2020-06-17 18:49:53 +0530

    Added Validation
    Implemented Validation.

--- webapp/components/Register.vue
+++ webapp/components/Register.vue
@@ -1,42 +1,109 @@
 <template>
-    <v-card>
-        <v-card-title>
-          <span class="headline">Sign Up!</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              
-              <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-col>
-              
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-          <p class="text-lg-right">Already an User?<v-btn @click="changeOption" text color="orange darken-1"> Log in </v-btn> </p>
-        </v-card-text>
-        <v-card-actions>
-          <!-- <v-spacer></v-spacer> -->
-          <v-btn color="orange darken-1" text @click="dialog = false" class="mx-auto">Sign Up</v-btn>
-        </v-card-actions>
-    </v-card>
+  <v-card>
+    <v-card-title>
+      <span class="headline">Sign Up!</span>
+    </v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+              ></v-text-field>
+            </v-col>
+            <!-- <v-col cols="12"> {{ name }} </v-col> -->
+            <v-col cols="12">
+              <v-text-field
+                label="Password*"
+                v-model="pass"
+                :rules="passRules"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                label="Confirm Password*"
+                v-model="pass2"
+                :rules="passTwoRules"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-container>
+      <small>*indicates required field</small>
+      <p class="text-lg-right">
+        Already an User?<v-btn
+          @click="changeOption"
+          text
+          color="orange darken-1"
+        >
+          Log in
+        </v-btn>
+      </p>
+    </v-card-text>
+    <v-card-actions>
+      <!-- <v-spacer></v-spacer> -->
+      <v-btn
+        color="orange darken-1"
+        text
+        class="mx-auto"
+        :disabled="!valid"
+        @click="validate"
+        >Sign up</v-btn
+      >
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        methods: {
-            changeOption(){
-                this.$emit('optionChanged');
-            }
-            
-        },
+export default {
+  data() {
+    return {
+      valid: true,
+      pass: '',
+      pass2: '',
+      passRules: [
+        (v) => !!v || 'Password is required',
+        (v) =>
+          (v && v.length <= 8) || 'Password must be less than 8 characters',
+        (v) => (v && this.checkMatch) || 'Passwords must match'
+      ],
+      passTwoRules: [
+        (v) => !!v || 'Password is required',
+        (v) =>
+          (v && v.length <= 8) || 'Password must be less than 8 characters',
+        (v) => (v && this.checkMatch) || 'Passwords must match'
+      ],
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ]
     }
+  },
+  methods: {
+    changeOption() {
+      this.$emit('optionChanged')
+    },
+    validate() {
+      this.$refs.form.validate()
+    }
+  },
+  computed: {
+    checkMatch() {
+      if (this.pass === this.pass2) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
