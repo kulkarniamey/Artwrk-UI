7fcf6bc110deb33d5b10e26feb736949e2cbb9cf
Soham Tamboli <sohamtamboli@gmail.com>
2020-07-03 00:22:10 +0530

    Finalized auth routes
    Removed testregi
    Created auth folder in pages
    Created seperate pages for login and registration
    Added user type in register form

---
+++ webapp/pages/auth/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="6"> <h2>Art is an explosion!</h2> </v-col>
+
+      <v-col cols="6">
+        <Login> </Login>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Login from '../../components/Login'
+export default {
+  name: 'loginPage',
+  components: {
+    Login
+  },
+  head() {
+    title: 'Log in'
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ webapp/pages/auth/register.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="6"> <h2>Art is an explosion!</h2> </v-col>
+
+      <v-col cols="6">
+        <Register> </Register>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Register from '../../components/Register'
+
+export default {
+  name: 'registerPage',
+  components: {
+    Register
+  },
+  head() {
+    title: 'Sign Up'
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
