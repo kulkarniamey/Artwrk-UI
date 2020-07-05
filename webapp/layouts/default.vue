3219b83e9512c3737842b398bbcaab4ca0df45ce
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2020-07-05 14:15:53 +0530

    Revert "Revert "Merge branch 'a-auth' of github.com:Artwrk/Artwrk-UI into a-auth""
    This reverts commit e67540c63ba9adb8bac5279c8fb7a30647d9e16c.

--- webapp/layouts/default.vue
+++ webapp/layouts/default.vue
@@ -18,17 +18,18 @@
 
       <v-spacer />
    
-   <div v-if="isLoggedIn">
-      <v-btn text large dark>Welcome User </v-btn>
-     <v-btn text @click="logOut" dark>Logout</v-btn>
+   <div v-if="$auth.loggedIn"> 
+     flag  = {{ $auth.user }}
+      <v-btn text large dark >Welcome User </v-btn>
+     <v-btn text @click="$auth.logout()" dark>Logout</v-btn>
    </div>
 
    <div v-else>
-     flag = {{ $auth.user }}
-
-     ><v-btn text  >Login</v-btn>
      
-     <v-btn text to="/testregi">Register</v-btn>
+
+     <v-btn text to="/auth/login">Login</v-btn>
+     
+     <v-btn text to="/auth/testregi">Register</v-btn>
    </div>
       
 
@@ -131,15 +132,13 @@ import Login from '../components/Login';
             },
 
             logOut:function(){
-              return $auth.logout()
+               this.$auth.logout()
             }
 
 
     },
 
-    components:{
-            Login
-        },
+  
 
     
   }
