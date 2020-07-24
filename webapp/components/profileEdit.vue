b025e984ea6079b97f8af2793a3b216935f87c87
ZeffK <54438151+ZeffK@users.noreply.github.com>
2020-07-24 13:59:28 +0530

    added text field to dialog

--- webapp/components/profileEdit.vue
+++ webapp/components/profileEdit.vue
@@ -21,30 +21,19 @@
           <v-container>
             <v-row>
               <v-col cols="12" sm="6" md="12">
-                <v-text-field label="Description of Company*" required></v-text-field>
+                <v-text-field label="Description of Company" required></v-text-field>
               </v-col>
               
               
               <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
+                <v-text-field label="Location " required></v-text-field>
               </v-col>
-              <v-col cols="12">
-                <v-text-field label="Password*" type="password" required></v-text-field>
+
+               <v-col cols="12">
+                <v-text-field label="Email " required></v-text-field>
               </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
+          
+      
             </v-row>
           </v-container>
           <small>*indicates required field</small>
