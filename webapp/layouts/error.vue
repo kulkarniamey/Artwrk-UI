30626ddcee5acd0eb5406c27535ba87ff6857ae1
Soham Tamboli <sohamtamboli@gmail.com>
2020-07-26 18:43:04 +0530

    ERRORPAGE ADDED

--- webapp/layouts/error.vue
+++ webapp/layouts/error.vue
@@ -1,14 +1,27 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
+    <div v-if="isError">
+      <div class="bg">
+        <div class="content">
+          <h1 class=" custom-size-h1">
+            404
+          </h1>
+          <h2 class="text-h5 font-weight-regular">
+            Looks like you stumbled upon a wrong art!
+          </h2>
+          <h2 class="text-h5 font-weight-regular">
+            We make no mistakes just happy little accidents.<br />
+            <h2 class="text-h5 text-right font-weight-regular">-bob ross.</h2>
+          </h2>
+          <v-btn class="ma-2" dark tile @click="goBack" color="#0000ff">
+            <v-icon left>mdi-arrow-left</v-icon>Home page
+          </v-btn>
+        </div>
+      </div>
+    </div>
     <h1 v-else>
       {{ otherError }}
     </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
   </v-app>
 </template>
 
@@ -24,7 +37,14 @@ export default {
   data() {
     return {
       pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      otherError: 'An error occurred',
+      isError: true
+    }
+  },
+  methods: {
+    goBack() {
+      this.isError = false
+      this.$router.push('/comingsoon')
     }
   },
   head() {
@@ -33,12 +53,34 @@ export default {
     return {
       title
     }
+  },
+  mounted() {
+    this.isError = true
   }
 }
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
+.bg {
+  background-image: url('/svgg.svg');
+  height: 100vh;
+  background-size: 100% 100%;
+  -o-background-size: 100% 100%;
+  -webkit-background-size: 100% 100%;
+  background-size: cover;
+  overflow: hidden;
+}
+
+.custom-size-h1 {
+  font-size: 18em;
+  color: #0000ff;
+  font-weight: 700;
+}
+
+.content {
+  position: absolute;
+  top: 25%;
+  left: 25%;
+  transform: translate(-30%, -30%);
 }
 </style>
