b30d39d1d557b6b2be6db393c586180fcba2b891
Soham Tamboli <sohamtamboli@gmail.com>
2020-09-03 13:39:36 +0530

    fixes in displaying name and username

--- webapp/components/CoverPic.vue
+++ webapp/components/CoverPic.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="text-center cover">
-      <v-avatar color="indigo" size="63"
-        ><span class="white--text headline">PS</span></v-avatar
+      <v-avatar color="indigo" size="64"
+        ><v-icon dark size="40">mdi-account</v-icon></v-avatar
       >
       <h3 class="white--text">{{ profile.username }}</h3>
-      <p class="white--text">{{ profile.user_id }}</p>
+      <p class="white--text">{{ profile.name }}</p>
     </div>
   </div>
 </template>
@@ -14,11 +14,34 @@
 export default {
   name: 'CoverPic',
   data: () => ({
-    profile: {}
+    profile: {},
+    isDpNull: true
   }),
   props: { profileData: { type: Object } },
   mounted() {
     this.profile = this.profileData
+    this.fetchDp()
+  },
+  methods: {
+    async fetchDp() {
+      const payload = {
+        operation: 'get_posts_by_user',
+        id: this.profile.user_id
+      }
+      const response = await this.$axios.put(
+        'https://cuwewf4fsg.execute-api.ap-south-1.amazonaws.com/artwrkInit/profile-picture',
+        payload
+      )
+      this.postData = response.data
+      console.log(this.postData?.posts?.length)
+      if (this.postData?.posts?.length) {
+        this.isPostsNull = false
+      } else {
+        this.isPostsNull = true
+      }
+
+      console.log(this.postData)
+    }
   }
 }
 </script>
