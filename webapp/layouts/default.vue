a33f9e98533eefd82822c1fabe9d0ce6a404e28e
Soham Tamboli <sohamtamboli@gmail.com>
2020-09-15 20:35:22 +0530

    notification and jobs button shows in mobile view

--- webapp/layouts/default.vue
+++ webapp/layouts/default.vue
@@ -72,7 +72,6 @@
                     <v-card-actions>
                       <v-spacer></v-spacer>
                     </v-card-actions>
-            
 
                     <v-card-actions>
                       <v-btn text nuxt :to="notification.actionLink">{{
@@ -116,6 +115,69 @@
           </div>
         </template>
         <template v-if="mobile">
+          <div v-if="$auth.loggedIn">
+            <v-btn color="indigo accent-4" to="/jobs" rounded> Jobs </v-btn>
+          </div>
+          <v-menu
+            v-if="$auth.loggedIn"
+            bottom
+            offset-y
+            transition="slide-x-transition"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                :disabled="notifications.length === 0"
+                dark
+                icon
+                class="ma-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-badge
+                  :content="notifications.length"
+                  :value="notifications.length"
+                  color="green"
+                  overlap
+                >
+                  <v-icon color="black">mdi-bell-outline</v-icon>
+                </v-badge>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-list>
+                <v-list-item
+                  v-for="(notification, i) in notifications"
+                  :key="i"
+                >
+                  <v-list-item-title
+                    >{{ notification.notification }}
+                  </v-list-item-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                  <v-list-item
+                    v-for="(notification, i) in notifications"
+                    :key="i"
+                  >
+                    <v-list-item-title
+                      >{{ notification.text }}
+                    </v-list-item-title>
+
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+
+                      <v-btn text nuxt :to="notification.actionLink">{{
+                        notification.actionText
+                      }}</v-btn>
+                      <v-btn color="primary" text @click="removeNotification(i)"
+                        >Remove</v-btn
+                      >
+                    </v-card-actions>
+                  </v-list-item>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-menu>
           <v-app-bar-nav-icon
             @click.stop="drawer = !drawer"
           ></v-app-bar-nav-icon>
@@ -134,66 +196,6 @@
           class="d-flex"
         >
           <v-list-itm-group v-if="$auth.loggedIn">
-            <v-list-item>
-              <v-menu bottom offset-y transition="slide-x-transition">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    :disabled="notifications.length === 0"
-                    dark
-                    icon
-                    class="ma-2"
-                    v-bind="attrs"
-                    v-on="on"
-                  >
-                    <v-badge
-                      :content="notifications.length"
-                      :value="notifications.length"
-                      color="green"
-                      overlap
-                    >
-                      <v-icon color="black">mdi-bell-outline</v-icon>
-                    </v-badge>
-                  </v-btn>
-                </template>
-                <v-card>
-                  <v-list>
-                    <v-list-item
-                      v-for="(notification, i) in notifications"
-                      :key="i"
-                    >
-                      <v-list-item-title
-                        >{{ notification.notification }}
-                      </v-list-item-title>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                      </v-card-actions>
-                      <v-list-item
-                        v-for="(notification, i) in notifications"
-                        :key="i"
-                      >
-                        <v-list-item-title
-                          >{{ notification.text }}
-                        </v-list-item-title>
-
-                        <v-card-actions>
-                          <v-spacer></v-spacer>
-
-                          <v-btn text nuxt :to="notification.actionLink">{{
-                            notification.actionText
-                          }}</v-btn>
-                          <v-btn
-                            color="primary"
-                            text
-                            @click="removeNotification(i)"
-                            >Remove</v-btn
-                          >
-                        </v-card-actions>
-                      </v-list-item>
-                    </v-list-item>
-                  </v-list>
-                </v-card>
-              </v-menu>
-            </v-list-item>
             <v-list-item>
               <v-icon>mdi-account</v-icon>
               <v-btn
@@ -242,7 +244,7 @@ import SiteLoader from '../components/SiteLoader'
 
 export default {
   props: {
-    source: String,
+    source: String
   },
   components: { FooterComponent, SiteLoader },
   data: () => ({
@@ -253,13 +255,13 @@ export default {
       {
         icon: 'mdi-apps',
         title: 'Welcome',
-        to: '/',
+        to: '/'
       },
       {
         icon: 'mdi-chart-bubble',
         title: 'Inspire',
-        to: '/inspire',
-      },
+        to: '/inspire'
+      }
     ],
     icons: [],
 
@@ -270,21 +272,21 @@ export default {
     text: 'Hello I am New User',
     timeout: null,
     notifications: [],
-    user: undefined,
+    user: undefined
   }),
   watch: {
     group() {
       this.drawer = false
-    },
+    }
   },
   computed: {
-    isLoggedIn: function () {
+    isLoggedIn: function() {
       return this.$auth.loggedIn
     },
 
-    logOut: function () {
+    logOut: function() {
       this.$auth.logout()
-    },
+    }
   },
   methods: {
     removeNotification(index) {
@@ -299,7 +301,7 @@ export default {
       } else {
         return this.$auth?.user?.user_id
       }
-    },
+    }
   },
   async created() {
     this.user = this.$auth?.user?.username || null
@@ -310,7 +312,7 @@ export default {
         const token = state.replace('Bearer ', '')
         const userPayload = {
           operation: 'get_profile',
-          authorizationToken: token,
+          authorizationToken: token
         }
         let user = await this.$axios
           .put(
@@ -324,7 +326,7 @@ export default {
               this.notifications.push({
                 notification: 'You have not yet verified your email',
                 actionText: 'verify',
-                actionLink: '/auth/verify',
+                actionLink: '/auth/verify'
               })
             }
           })
@@ -338,9 +340,9 @@ export default {
       const payload = {
         operation: 'get_all_notifications',
         id: this.checkAdmin(),
-        authorizationToken: this.$auth.getToken('local').replace('Bearer ', ''),
+        authorizationToken: this.$auth.getToken('local').replace('Bearer ', '')
       }
-      
+
       try {
         const response = await this.$axios.put(
           `https://cuwewf4fsg.execute-api.ap-south-1.amazonaws.com/artwrkInit/notifications`,
@@ -361,7 +363,7 @@ export default {
   },
   mounted() {
     this.mobile = screen.width > 760 ? false : true
-  },
+  }
 }
 </script>
 <style scoped>
