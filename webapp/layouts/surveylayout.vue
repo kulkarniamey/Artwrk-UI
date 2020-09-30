fc2e1b26cab2327e7f74dce1436187b226560956
Soham Tamboli <sohamtamboli@gmail.com>
2020-09-30 13:08:23 +0530

    analytics added

--- webapp/layouts/surveylayout.vue
+++ webapp/layouts/surveylayout.vue
@@ -11,6 +11,28 @@
 </template>
 
 <script>
+;(function(v, i, s, a) {
+  if (!v._visaSettings) {
+    v._visaSettings = {}
+  }
+  v._visaSettings['e4e3b3d9-0244-11eb-b589-901b0edac50a'] = {
+    v: '0.3',
+    s: 'e4e3b3d9-0244-11eb-b589-901b0edac50a',
+    a: '1'
+  }
+  _v = i.getElementsByTagName('head')[0]
+  _a = _v
+  _i = i.createElement('script')
+  _s = _i
+  _s.defer = 'defer'
+  _s.src = s + a + v._visaSettings['e4e3b3d9-0244-11eb-b589-901b0edac50a'].v
+  _a.appendChild(_s)
+})(
+  window,
+  document,
+  '//app-worker.visitor-analytics.io/main',
+  '.js?s=e4e3b3d9-0244-11eb-b589-901b0edac50a&v='
+)
 export default {
   props: {
     source: String
